4d6bb1fe1a10be7d84c6587780eca4d6da82db2b
angeli <unjhaylee122301@gmail.com>
2023-04-26 22:33:27 +0800

    second commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import NavBar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/Homepage';
+import Login from './pages/Login';
+import Registration from './pages/Registration';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/home" element={<HomePage />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/registration" element={<Registration />}/>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1 align='center'>Welcome to my homepage!</h1>
+      <p align='center'>Here is some content that I want to display on my homepage.</p>
+    </div>
+  );
+};
+
+export default Homepage;
