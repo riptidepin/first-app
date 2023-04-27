383633f48b3387c65c4d1a54afd59915557be802
riptidepin <unjhaylee122301@gmail.com>
2023-04-27 09:36:24 +0800

    with cute login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,43 +1,72 @@
-import React, { useState } from 'react';
+import React from 'react';
+import styled from 'styled-components';
 
-const LoginPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const Container = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
+background-color: #fce4ec; /* Pink background color */
+`;
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(`Email: ${email}, Password: ${password}`);
-    // Perform login action here
-  };
+const Form = styled.form`
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 2rem;
+background-color: #fff;
+border-radius: 0.5rem;
+box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.3);
+`;
 
-  return (
-    <div className="container">
-      <form onSubmit={handleSubmit} className="login-form">
-        <h1 align = 'center' className="title">Login</h1>
-        <div align = 'center' className="form-group">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-            required
-          />
-        </div>
-        <div align = 'center' className="form-group">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            required
-          />
-        </div>
-        <button align = 'center' type="submit" className="submit-button">Login</button>
-      </form>
-    </div>
+const Title = styled.h1`
+font-size: 2rem;
+margin-bottom: 2rem;
+`;
+
+const Label = styled.label`
+font-size: 1.2rem;
+margin-bottom: 1rem;
+`;
+
+const Input = styled.input`
+font-size: 1.2rem;
+padding: 0.5rem;
+margin-bottom: 2rem;
+border: none;
+border-bottom: 2px solid #555;
+outline: none;
+&:focus {
+border-color: #e91e63; /* Pink border color on focus */
+}
+`;
+
+const Button = styled.button`
+font-size: 1.2rem;
+padding: 0.5rem 2rem;
+background-color: #e91e63; /* Pink button color */
+color: #fff;
+border: none;
+border-radius: 0.5rem;
+cursor: pointer;
+&:hover {
+background-color: #ff69b4; /* Lighter pink button color on hover */
+}
+`;
+
+const Login = () => {
+return (
+    <Container>
+      <Form>
+        <Title>Login</Title>
+        <Label htmlFor="username">Username</Label>
+        <Input type="text" id="username" name="username" required/>
+        <Label htmlFor="password">Password</Label>
+        <Input type="password" id="password" name="password" required/>
+        <Button type="submit">Submit</Button>
+      </Form>
+    </Container>
   );
 };
 
-export default LoginPage;
+export default Login;
