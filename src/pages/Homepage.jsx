4fdc17047ff5a6dc4b632569adf6254cc624b270
riptidepin <unjhaylee122301@gmail.com>
2023-04-27 09:28:01 +0800

    with image

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
+import myImage from '../Image/Barbie.png';
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+height: 100vh;
+background-image: url('https://images.unsplash.com/photo-1523058467403-8d7b75e87336');
+background-size: cover;
+`;
+
+const Title = styled.h1`
+font-size: 3rem;
+font-weight: bold;
+margin-bottom: 2rem;
+color: #fd71ae;
+text-shadow: 2px 2px #ff99cc;
+`;
+
+const Content = styled.p`
+font-size: 1.5rem;
+text-align: center;
+line-height: 2rem;
+color: #fea9ca;
+text-shadow: 2px 2px #ff99cc;
+`;
+
 
 const Homepage = () => {
-  return (
-    <div>
-      <h1 align='center'>Welcome to my homepage!</h1>
-      <p align='center'>Here is some content that I want to display on my homepage.</p>
-    </div>
-  );
+return (
+  <Container>
+    <Title>Hello!</Title>
+    <Container className="App">
+      <img src={myImage} className="App-logo" alt='My Image' />
+    </Container>
+    <Content>
+      Welcome to my page 
+    </Content>
+  </Container>
+);
 };
 
 export default Homepage;
